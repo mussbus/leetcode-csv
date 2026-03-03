bd043d5fcdf50ad52fb393275c453c494df9788c
mussbus <chadmusser98@gmail.com>
2026-03-03 08:57:56 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from leetcode-csv-updater!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ update.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import cast
+
+import pandas as pd
+
+cwd: Path = Path.cwd().resolve() / "input"
+files = Path(cwd).glob("*.csv")
+dfs = (cast(pd.DataFrame, pd.read_csv(f)) for f in files)
+df: pd.DataFrame = pd.concat(dfs, ignore_index=True)
+
+df["date"] = pd.to_datetime(df["date"])
+df["score"] = pd.to_numeric(df["score"], errors="raise")
+
+df = df.sort_values(["category", "date"])
+
+df["cumulative_score"] = df.groupby("category")["score"].cumsum()
+
+# print(df[["date", "category", "score", "cumulative_score"]])
+
+output_dir = Path("dist")
+output_dir.mkdir(exist_ok=True)
+
+for date, group in df.groupby("date"):
+    snapshot = (
+        df[df["date"] <= date]
+        .sort_values("date")
+        .groupby("category", as_index=False)
+        .last()[["category", "cumulative_score"]]
+    )
+
+    out_path = output_dir / f"leetcode_totals_{date.date()}.csv"
+    snapshot.to_csv(out_path, index=False)
